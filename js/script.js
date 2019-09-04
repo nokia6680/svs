3b78c407e161d0fc9f41d4e0d6e527447f6809b6
Artur Ballod <artur.ballod@gmail.com>
2019-09-04 11:22:23 +0200

    #part

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 	});
 });
+
+$(".intro-slider").slick({
+  infinite: true,
+  initialSlide: 0,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: false,
+	autoplay: true,
+  prevArrow: '<div class="prev"></div>',
+  nextArrow: '<div class="next"></div>'
+});
