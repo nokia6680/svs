79c88e920e8d465e112745734af8913908704178
nokia6680 <zenitfan@yandex.ru>
2019-10-10 02:41:50 +0200

    #index-catalog-tablet

--- js/slider.js
+++ js/slider.js
@@ -1,37 +1,17 @@
-$(".intro-slider").slick({
-  infinite: true,
-  initialSlide: 0,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  dots: false,
-	autoplay: true,
-	fade: true,
-  prevArrow: '<div class="prev"></div>',
-  nextArrow: '<div class="next"></div>'
-});
+var swiper = new Swiper('.swiper-container', {
+  autoplay: true,
+  loop: true,
 
-$(".card-slider__wrapper").slick({
-  infinite: true,
-  initialSlide: 0,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: true,
-  dots: false,
-	autoplay: true,
-  prevArrow: '<div class="prev-1"></div>',
-  nextArrow: '<div class="next-1"></div>',
-  slideCount: '<span class="card-slider__counter-overall"></span>',
-  slickCurrentSlide: '<span class="card-slider__counter-current"></span>'
-});
-
-// Элемент, куда вы хотите записать страницы
-let pages = $('.card-slider__counter');
-// Элемент слайдера
-let slider = $('.card-slider__wrapper');
-
-slider.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
-
-   let i = (currentSlide ? currentSlide : 0) + 1;
-    pages.text(i + '/' + slick.slideCount);
+  breakpoints: {
+    768: {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+      },
+    },
+  },
 });
