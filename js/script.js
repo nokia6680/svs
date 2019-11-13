9b0a513bbba0288a9925a7aa8becb3afff189c61
nokia6680 <zenitfan@yandex.ru>
2019-11-13 22:04:55 +0200

    ###csc

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 /*Menu*/
-//const SearchDeskOpen = document.querySelector('.nav-service__search');
-//const SearchDeskField = document.querySelector('.nav-service__form');
-//const SearchDeskClose = document.querySelector('.nav-service__input-pic--close');
+const SearchDeskOpen = document.querySelector('.nav-service__search');
+const SearchDeskField = document.querySelector('.nav-menu__search');
+const SearchDeskClose = document.querySelector('.nav-menu__search-close');
+const SiteMenu = document.querySelector('.nav-menu__links');
+const NavMenu = document.querySelector('.nav-menu');
+const SearchRes = document.querySelector('.search-result');
+
 const MenuButton = document.querySelector('.header-nav__toggle');
 const MobileMenu = document.querySelector('.nav-menu');
 const MenuWrap = document.querySelector('.header');
@@ -12,13 +16,19 @@ const CatView = document.querySelector('.catalog-filter__toggle');
 const CatList = document.querySelector('.catalog-list');
 /*Variables for callback popup*/
 
-//document.querySelector('.nav-service__search').onclick = function () {
-  //SearchDeskField.classList.add('active');
-//};
+document.querySelector('.nav-service__search').onclick = function () {
+  SearchDeskField.classList.add('active');
+	SiteMenu.classList.add('active');
+	NavMenu.classList.add('active');
+	SearchRes.classList.add('active');
+};
 
-//document.querySelector('.nav-service__input-pic--close').onclick = function () {
-//  SearchDeskField.classList.remove('active');
-//};
+document.querySelector('.nav-menu__search-close').onclick = function () {
+  SearchDeskField.classList.remove('active');
+	SiteMenu.classList.remove('active');
+	NavMenu.classList.remove('active');
+	SearchRes.classList.remove('active');
+};
 
 document.querySelector('.header-nav__toggle').onclick = function(){
 	MenuButton.classList.toggle('open');
@@ -78,3 +88,9 @@ function openorder(id) {
       div.style.display = 'flex';
   }
 };
+
+// Смена текста кнопки развернуть-свернуть
+function toggleText(button_id)  {
+   var text = document.getElementById('btn-expand').firstChild;
+   text.data = text.data == "Развернуть все" ? "Свернуть все" : "Развернуть все";
+}
