2f640607045a5b99851f3ce1c75e28236c243032
nokia6680 <zenitfan@yandex.ru>
2020-05-01 02:13:15 +0200

    ку

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,44 @@
+// breakpoint where swiper will be destroyed
+// and switches to a dual-column layout
+const breakpoint = window.matchMedia( '(min-width: 768px)' );
+// keep track of swiper instances to destroy later
+let mySwiper;
+const breakpointChecker = function() {
+   // if larger viewport and multi-row layout needed
+   if ( breakpoint.matches === true ) {
+      // clean up old instances and inline styles when available
+      if ( mySwiper !== undefined ) mySwiper.destroy( true, true );
+      // or/and do nothing
+      return;
+   // else if a small viewport and single column layout needed
+   } else if ( breakpoint.matches === false ) {
+      // fire small viewport version of swiper
+      return enableSwiper();
+   }
+};
+
+const enableSwiper = function() {
+   mySwiper = new Swiper ('.swiper-options', {
+      loop: true,
+      slidesPerView: 1.2,
+      centeredSlides: true,
+      a11y: true,
+      keyboardControl: true,
+      grabCursor: true
+   });
+};
+
+// keep an eye on viewport size changes
+breakpoint.addListener(breakpointChecker);
+// kickstart
+breakpointChecker();
+
 var swiper = new Swiper('.swiper-container', {
   autoplay: true,
   loop: true,
 
   breakpoints: {
-    768: {
+    320: {
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
