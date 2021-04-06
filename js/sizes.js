4ee30acc04100f7f955758f1b68b7f398f2ceef9
Artur Ballod <artur.ballod@gmail.com>
2021-04-06 15:16:50 +0200

    фывыфвфы

--- js/sizes.js
+++ js/sizes.js
@@ -1,11 +1,10 @@
-var sizesBtn = document.getElementsByClassName('item-color__toggle');
+var upperLvl = document.getElementsByClassName('item-color__toggle');
 var elNodes = document.querySelectorAll(".item-color__toggle");
-var sizeList = document.getElementsByClassName('item-color__list');
 
-for(var i = 0; i < sizesBtn.length; i++) {
-  var elem = sizesBtn[i];
+for(var i = 0; i < upperLvl.length; i++) {
+  var elem = upperLvl[i];
 
   elem.addEventListener("click", function(){
-    sizeList.classList.toggle("active");
+    this.classList.toggle("active");
   });
 };
